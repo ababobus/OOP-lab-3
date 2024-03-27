7218a3c16160a1362837056b91473d9d69778d57
Азалия Баймуратова <ozalea@yandex.ru>
2024-03-27 18:17:51 +0500

    added getsize, position, getvalue, pop_back

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -36,15 +36,99 @@ public:
         std::cout << "~MyStorage\n";
     }
 
+    int get_size() {//размер списка
+        return size;
+    }
 
-    void push_front(T object) {
+    T take_front() { //получение первого элемент списка
+        if (size != 0) {
+            return front->data;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    T pop_front() {//удаление первого элемента списка
+        T value = first->data;
+
+        if (size == 1) {
+            delete front;
+            front = nullptr;
+            back = nullptr;
+            size--;
+            return value;
+        }
+
+        if (size == 0) {
+            throw std::out_of_range("empty list");
+            return 0;
+        }
+
+        front = front->next;
+        delete front->left;
+        front->next = nullptr;
+        size--;
+        return value;
+    }
+
+    T Take_back() {//получение последнего элемента списка
+        if (size != 0) {
+            return back->data;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    T pop_back() {//удаление последнего элемента списка
+        T value = back->data;
+
+        if (size == 1) {
+            delete back;
+            front = nullptr;
+            back = nullptr;
+            size--;
+            return value;
+        }
+
+        if (size == 0) {
+            throw std::out_of_range("empty list");
+            return 0;
+        }
+
+        back = back->prev;
+        delete back->next;
+        back->next = nullptr;
+        size--;
+        return value;
+    }
+
+    T *position(int index) { //получение указателя на объект по индексу (для получения объекта по индексу)
+        MyBase<T> *buf = front;
+        for (int i = 0; i < size; i++) {
+            if (i == index) {
+                return buf;
+            }
+            else {
+                buf = buf->next;
+            }
+        }
+        return nullptr;
+    }
+
+    T get_value(int index) { //получение объекта по индексу
+        return position(index)->data;
+    }
+
+    void push_front(T object) {//вставка элемента в начало 
         MyBase<T> * buf = new MyBase<T>(object);
         buf->data = object;
         buf->front = nullptr;
         buf->back = front; //одностороння связь буф и фронта (0 и 1 элем)
 
         if (size > 0) {
-            front->prev = nullptr; //двсторонняя связь
+            front->prev = nullptr; //двусторонняя связь
             front = buf; //перетаскиваем фронт
         }
         else {
@@ -53,7 +137,7 @@ public:
         size++;
     }
 
-    void push_back(T object) {
+    void push_back(T object) {//вставка элемента в конец
         MyBase<T>* buf = new MyBase<T>;
         buf->data = object;
         buf->front = back;
@@ -69,7 +153,7 @@ public:
         size++;
     }
 
-    void insert(int index, T object) {
+    void insert(int index, T object) {//вставка элемента по индексу
         if (index == 0) {
             push_front(value);
             return;
@@ -79,7 +163,7 @@ public:
             return;
         }
         if (index < 0 || index > size) {
-            cout << "out of range";
+            throw std::out_of_range("err: wrong index");
             return;
         }
 
@@ -91,12 +175,13 @@ public:
 
         MyBase<T>* old = buf; //принимает значение элемента с заданным индексом
         buf = new MyBase<T>(object);
-        buf->prev = old->prev;
-        buf->next = old;
+        buf->prev = old->prev; 
+        buf->next = old; //односторонняя связь
 
-
-
-
+        old->prev->next = buf;//двусторонняя связь
+        old->prev = buf;
+        
+        size++;
     }
 
     
